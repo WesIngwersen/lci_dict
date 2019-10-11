2c7fc3e3e305e522ff615aef57622572841e19b1
WesIngwersen <ingwersen.wesley@epa.gov>
2019-10-11 16:46:21 -0400

    Add process metadata template

--- lci_dict/globals.py
+++ lci_dict/globals.py
@@ -8,7 +8,6 @@ except NameError:
 
 outputpath = modulepath + 'output/'
 
-bdata_folder = modulepath + '../data-background/'
 default_location = 'US'
 naics_category = 'NAICS-2/NAICS-4'
 default_product_flow_category = ''
@@ -16,5 +15,5 @@ default_product_flow_category = ''
 default_unit_name = 'Item(s)'
 
 #Read in general metadata to be used by all processes
-metadata = pd.read_csv(bdata_folder + 'process_metadata_template.csv')
+metadata = pd.read_csv(modulepath + 'process_metadata_template.csv')
 
